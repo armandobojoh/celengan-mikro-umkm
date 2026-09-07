3cd285a115ffda67a1a8aff16307efe67b1c4b93
armandobojoh <12410194@student.unklab.ac.id>
2026-09-08 06:39:56 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'https://celengan-mikro-umkm-production.up.railway.app';
+const API_BASE = 'https://celengan-mikro-umkm-production.up.railway.app/api';
 
 async function apiFetch(path, options = {}) {
   const token = localStorage.getItem('modalrt_token');
